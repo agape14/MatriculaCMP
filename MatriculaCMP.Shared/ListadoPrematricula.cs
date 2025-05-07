1297956318626034557b00b1ec85edd5f6def179
agape14 <adelacruzcarlos@gmail.com>
2025-05-07 17:29:26 -0500

    formularios de seguimiento para solicitante

---
+++ MatriculaCMP.Shared/ListadoPrematricula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaCMP.Shared
+{
+    public class ListadoPrematricula
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public string NombreCompleto { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string LugarNacimiento { get; set; }
+        public string Estado { get; set; } // "Pagado", "sin pagar", "Desistido", etc.
+        public bool Seleccionado { get; set; } // <-- NUEVO
+    }
+}
---
+++ MatriculaCMP/MatriculaCMP.Client/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿window.confirmarSweet = async function (titulo, texto) {
+    const result = await Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+    });
+    return result.isConfirmed;
+};
+
+window.mostrarModal = (id) => {
+    console.log('muestra el moda', id);
+    var modal = new bootstrap.Modal(document.querySelector(id));
+    modal.show();
+};
+
+window.ocultarModal = (id) => {
+    var modalEl = document.querySelector(id);
+    var modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
+};
+
+window.inicializarDataTable = (tableId) => {
+    setTimeout(() => {
+        if (!$.fn.DataTable.isDataTable(tableId)) {
+            $(tableId).DataTable({
+                responsive: true,
+                language: {
+                    url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json"
+                }
+            });
+        }
+    }, 100);
+};
